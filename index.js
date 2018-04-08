b8b168591dcffc6e8c769045b914decefd24b97f
williamyeny <williamyeny@gmail.com>
2018-04-08 16:50:42 -0400

    added starter code

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var port = 3000;
+
+app.get('/', function(req, res){
+    res.render("index");
+});
+
+app.listen(port, function(){
+    console.log("Running on " + port);
+  });
