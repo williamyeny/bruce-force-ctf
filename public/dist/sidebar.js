ada15b0fdab0eafe1d2e92cfc6f3f154b671bce5
williamyeny <williamyeny@gmail.com>
2018-04-11 01:50:57 -0400

    improved expand collapse

--- public/dist/sidebar.js
+++ public/dist/sidebar.js
@@ -3,5 +3,6 @@ var challenges = document.getElementsByClassName("challenge-title");
 for (i = 0; i < challenges.length; i++) {
   challenges[i].addEventListener("click", function () {
     this.nextSibling.classList.toggle("show");
+    this.classList.toggle("selected");
   });
 }
