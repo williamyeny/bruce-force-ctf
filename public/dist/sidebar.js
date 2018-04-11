2f616e7115d039733be0eb033f432f0f23e30214
williamyeny <williamyeny@gmail.com>
2018-04-11 02:11:00 -0400

    hiding and showing sidebar

--- public/dist/sidebar.js
+++ public/dist/sidebar.js
@@ -1,8 +1,25 @@
-var challenges = document.getElementsByClassName("challenge-title");
+var cTitle = document.getElementsByClassName("challenge-title");
 
-for (i = 0; i < challenges.length; i++) {
-  challenges[i].addEventListener("click", function () {
+for (i = 0; i < cTitle.length; i++) {
+  cTitle[i].addEventListener("click", function () {
     this.nextSibling.classList.toggle("show");
     this.classList.toggle("selected");
   });
 }
+
+document.getElementsByClassName("showhide")[0].addEventListener("click", function() {
+  document.getElementsByClassName("sidebar")[0].classList.toggle("sidebar-hide");
+
+  var challenges = document.getElementsByClassName("challenge");
+  // hide challenges
+  for (i = 0; i < challenges.length; i++) {
+    challenges[i].classList.toggle("hide");
+  }
+  
+  // toggle text
+  if (document.getElementsByClassName("showhide")[0].innerHTML == "hide") {
+    document.getElementsByClassName("showhide")[0].innerHTML = "show";
+  } else {
+    document.getElementsByClassName("showhide")[0].innerHTML = "hide";
+  }
+})
