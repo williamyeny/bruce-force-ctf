979c58be4ec1f61e317a49d50138ff4cbdbd295d
Joyce <joycechoi19@gmail.com>
2018-04-10 21:29:32 -0400

    user id misalignment

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Review = mongoose.model('Review');
 
 exports.addReview = async (req, res) => {
-  //req.body.author = req.user._id;
-  req.body.author = req.params.user_id;
+  req.body.author = req.user._id;
+  // req.body.author = req.params.user_id;
   req.body.store = req.params.id;
   const newReview = new Review(req.body);
   await newReview.save();
