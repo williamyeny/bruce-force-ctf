75e39dd7f9eee013dfd29494bd48a8e00d81f339
williamyeny <williamyeny@gmail.com>
2018-04-11 20:51:04 -0400

    answer saving

--- public/dist/sidebar.js
+++ public/dist/sidebar.js
@@ -1,3 +1,15 @@
+var completedRaw = localStorage.getItem("bfctfCompleted");
+var completed = [];
+var challenges = document.getElementsByClassName("challenge");
+
+if (completedRaw != undefined) { // not first visit!
+  completed = JSON.parse(completedRaw);
+  console.log(completed);
+  for (i = 0; i < completed.length; i++) {
+    challenges[completed[i]].firstChild.innerHTML += " ✓";
+  }
+}
+
 var cTitles = document.getElementsByClassName("challenge-title");
 
 for (i = 0; i < cTitles.length; i++) {
@@ -10,7 +22,6 @@ for (i = 0; i < cTitles.length; i++) {
 document.getElementsByClassName("showhide")[0].addEventListener("click", function() {
   document.getElementsByClassName("sidebar")[0].classList.toggle("sidebar-hide");
 
-  var challenges = document.getElementsByClassName("challenge");
   // hide challenges
   for (i = 0; i < challenges.length; i++) {
     challenges[i].classList.toggle("hide");
@@ -26,9 +37,22 @@ document.getElementsByClassName("showhide")[0].addEventListener("click", functio
 
 var cAnswers = document.getElementsByClassName("answer-button");
 for (i = 0; i < cAnswers.length; i++) {
+  cAnswers[i].HTMLindex = i;
   cAnswers[i].addEventListener("click", function() {
-    if (this.previousSibling.value == this.parentElement.parentElement.getAttribute("data-answer")) {
-      alert("ay!");
+    if (completed.indexOf(this.HTMLindex) > -1) {
+      alert("You already completed this, dummy!");
+    } else {
+          
+      if (this.previousSibling.value == this.parentElement.parentElement.getAttribute("data-answer")) {
+        completed.push(this.HTMLindex);
+        console.log(completed);
+        localStorage.setItem("bfctfCompleted", JSON.stringify(completed));
+        alert("Correct!");
+        challenges[this.HTMLindex].firstChild.innerHTML += " ✓";
+      } else {
+        alert("That wasn't correct :(");
+        console.log("u wrong lol");
+      }
     }
   })
 }
